f542aba8f0a030da935b23c2657c1b124ecb3124
Duncan Smith <DuncSmith@users.noreply.github.com>
2023-04-07 15:31:52 +0100

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 #
-git clone --bare https://github.com/DuncSmith/config.git $HOME/.cfg
+git clone --bare https://github.com/DuncSmith/config.git "$HOME"/.cfg
 
 function config {
-   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+   /usr/bin/git --git-dir="$HOME"/.cfg/ --work-tree="$HOME" $@
 }
 
 mkdir -p .config-backup
-config checkout
-if [ $? = 0 ]; then
+if config checkout; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    conflict_files=$(config checkout 2>&1 | grep -E "\s+\." | awk {'print $1'})
+    
+    echo "$conflict_files" | xargs -n1 dirname | uniq | xargs -I{} mkdir -p .config-backup/{}
+    echo "$conflict_files" | xargs -I{} mv {} .config-backup/{}
 fi;
 config checkout
 config config status.showUntrackedFiles no
